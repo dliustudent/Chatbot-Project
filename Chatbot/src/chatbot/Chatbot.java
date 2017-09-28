ab9c826f94ea9067584a966576703bb16ae986f0
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-09-28 15:34:18 -0400

    Project

---
+++ Chatbot/src/chatbot/Chatbot.java
@@ -0,0 +1,31 @@
+package chatbot;
+
+public class Chatbot {
+
+	private String username;
+	private boolean chatting;
+	private Topic Derek;
+	
+	public Chatbot() {
+		Derek = new ChatbotDerek();
+		username = "Unknown User";
+		chatting = true;
+		
+	}
+
+	public void startChatting() {
+		ChatbotMain.print("Hi! I am an inteligent machine that can respond to your input. What is your name?");
+		username = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What would you like to talk about?");
+			String response = ChatbotMain.getInput();
+			if(Derek.isTriggered(response)) {
+				chatting = false; //exits the while loop, IMPORTANT FOR GRADE
+				Derek.talk(response);
+			}else {
+				ChatbotMain.print("I'm sorry. I don't understand. I never said i was perfect.");
+			}
+		}
+	}
+
+}
---
+++ Chatbot/src/chatbot/ChatbotDerek.java
@@ -0,0 +1,19 @@
+package chatbot;
+
+public class ChatbotDerek implements Topic {
+
+	public ChatbotDerek() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isTriggered(String response) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void talk(String response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
