42fdb1ff17ea30b98282645eb4458f11433d3be2
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-09-29 15:31:10 -0400

    Vincent Add

---
+++ Chatbot/src/chatbot/ChatbotVincent.java
@@ -0,0 +1,45 @@
+package chatbot;
+
+public class ChatbotVin implements Topic {
+
+	private String[] keywords;
+	private String goodbyeKeyword;
+	private String secretKeyword;
+	private String response;
+	
+	
+	public ChatbotVin() {
+		String[] temp = {"stuff","things","whatever","nothing"};
+		keywords = temp;
+		goodbyeKeyword = "bye";
+		secretKeyword = "pug";
+		response = "";
+	}
+	
+	public void talk(String response) {
+		ChatbotMain.print("Hey! So you want to talk about, ...");
+		response = ChatbotMain.getInput();
+		while(!response.equals(goodbyeKeyword)) {
+				if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
+					ChatbotMain.print("I can't even. I love pugs so much.");
+					response = ChatbotMain.getInput();
+				}else {
+				ChatbotMain.print("Yeah.Cool");
+				response = ChatbotMain.getInput();
+				}
+		}
+		//access variable from other classes
+		ChatbotMain.print("Well it was nice talking to you, "+ ChatbotMain.chatbot.getUsername()+"!");
+	}
+	
+	public boolean isTriggered(String response) {
+		for(int i = 0; i < keywords.length ; i++ ){
+			if(ChatbotMain.findKeyword(response, keywords[i],0) >=0) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+}
+
