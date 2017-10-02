8218e6985c3096256f73032a761ffcc899bcf26e
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-10-02 14:34:23 -0400

    Finish negations

--- Chatbot/src/chatbot/ChatbotMain.java
+++ Chatbot/src/chatbot/ChatbotMain.java
@@ -8,88 +8,116 @@ public class ChatbotMain {
 	private static Scanner inputSource = new Scanner(System.in);
 	//use static only if it is the same everywhere, wants it to be accessed independent of instances
 	
-	public static void main(String[] args) {
-	chatbot.startChatting();
-		  
-	} 
-	
-	// copied from classwork Utilities
-	/*
-	 * @return the index of the keyword after startPsn that is isolated and has no negations or -1 otherwise
-	 */
-	
-	public static int findKeyword(String searchString, String keyword, int startPsn) {
-		//make lower case
-		searchString = searchString.toLowerCase();
-		keyword = keyword.toLowerCase();
+		public static void main(String[] args) {
+		chatbot.startChatting();
+			  
+		} 
 		
-		int psn = searchString.indexOf(keyword, startPsn);
+		// copied from classwork Utilities
+		/*
+		 * @return the index of the keyword after startPsn that is isolated and has no negations or -1 otherwise
+		 */
 		
-		while (psn >= 0) {
-			if (keywordIsIsolated(psn,keyword,searchString) && noNegations(searchString, psn)) {
-				return psn;
-			}else {
-				psn = searchString.indexOf(keyword, psn+1);//returns the index of the NEXT keyword
-			}
-		}
-			return -1;
+		public static int findKeyword(String searchString, String keyword, int startPsn) {
+			//make lower case
+			searchString = searchString.toLowerCase();
+			keyword = keyword.toLowerCase();
 			
-	}
-
-	public static boolean keywordIsIsolated(int psn, String keyword, String s){
-			return true;
+			int psn = searchString.indexOf(keyword, startPsn);
+			
+			while (psn >= 0) {
+				if (keywordIsIsolated(psn,keyword,searchString) && noNegations(searchString, psn)) {
+					return psn;
+				}else {
+					psn = searchString.indexOf(keyword, psn+1);//returns the index of the NEXT keyword
+				}
+			}
+				return -1;
+				
 		}
-
+	
+		public static boolean keywordIsIsolated(int psn, String keyword, String s){
+			keyword = keyword.toLowerCase();
+			String wordAfter = "";
+			s = s.toLowerCase();
+			
+			try {
+				wordAfter = s.substring(psn+keyword.length()+1,psn+keyword.length()+2);
+			}catch(Exception ex) {
+				wordAfter = "";
+			}
+			if(wordAfter.equals("") || wordAfter.compareTo("z") < -26) {
+				return true;
+			}else {
+				return false;
+			}
+			
+		}
+	
 		public static boolean noNegations(String s, int psn){
-			return true;
+			int secondSpace = psn -1;
+			int firstSpace = -1;
+			for(int i = 0; i < secondSpace; i++) {
+				if(s.substring(i,i+1).compareTo(" ") == 0) {
+					firstSpace = i;
+				}
+			}
+			if(firstSpace == -1) {
+				return true;
+			}
+			String word = s.substring(firstSpace+1,secondSpace);
+			if(word.equals("not") || word.equals("no")){
+				return false;
+			}
+				return true;
 		}
-
-
+	
+	
 		public static String getInput(){
 			return inputSource.nextLine();
 		}
-
+	
 		public static void print(String s){
 			multiLinePrint(s);
 		}
-
+	
 		public static void multiLinePrint(String s){
 			String printString = "";
 			int cutoff = 25;
 			//this while loop last as long as there are words left in the original String
 			while(s.length() > 0){
-
+	
 				String currentCut = "";
 				String nextWord = "";
-
+	
 				//while the current cut is still less than the line length 
 				//AND there are still words left to add
 				while(currentCut.length()+nextWord.length() < cutoff && s.length() > 0){
-
+	
 					//add the next word
 					currentCut += nextWord;
-
+	
 					//remove the word that was added from the original String
 					s = s.substring(nextWord.length());
-
+	
 					//identify the following word, exclude the space
 					int endOfWord = s.indexOf(" ");
-
+	
 					//if there are no more spaces, this is the last word, so add the whole thing
 					if(endOfWord == -1) {
 						endOfWord = s.length()-1;//subtract 1 because index of last letter is one les than length
 					}
-
+	
 					//the next word should include the space
 					nextWord = s.substring(0,endOfWord+1);
 				}
-
+	
 				printString +=currentCut+"\n";
-
+	
 			}
 			System.out.print(printString);
 		}
-
+	
 		public static int getIntegerInput() {
 			print("Please enter an integer.");
 			String integerString = getInput();
@@ -107,7 +135,6 @@ public class ChatbotMain {
 			}
 			return value;
 		}
-
 	}
 
 
