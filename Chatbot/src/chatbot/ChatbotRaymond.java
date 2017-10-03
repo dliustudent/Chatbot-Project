85ec9a631786bf2b67331e07e4787f3cdc287bc6
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-10-03 15:33:46 -0400

    10/3

--- Chatbot/src/chatbot/ChatbotRaymond.java
+++ Chatbot/src/chatbot/ChatbotRaymond.java
@@ -8,10 +8,14 @@ public class ChatbotRaymond implements Topic {
 	private String[] insults;
 	private String secretKeyword;
 	private String response;
+	private String favInsect;
+	private String[] phrases;
+	private int madCount;
+	private int happyCount;
 	boolean vowelsFound;
 	
 	public ChatbotRaymond() {
-		String[] temp = {"insects","bugs","flys","ant","butterfly","bee","mosquito"};
+		String[] temp = {"insects","bugs","flys","ant","butterfly","bee","mosquito", "insect"};
 		keywords = temp;
 		
 		String[] tempVowels = {"a","e","i","o","u"};
@@ -20,7 +24,13 @@ public class ChatbotRaymond implements Topic {
 		String[] tempInsults = {"stupid", "idiot", "dumb"};
 		insults = tempInsults;
 		
+		String[] tempPhrases = {"Would you like to hear a fun fact?"};
+		
 		vowelsFound = false;
+		
+		favInsect = "";
+		madCount = 0;
+		happyCount = 0;
 		
 		goodbyeKeyword = "bye";
 		secretKeyword = "pug";
@@ -29,19 +39,27 @@ public class ChatbotRaymond implements Topic {
  
 	@Override
 	public void talk(String response) {
-		ChatbotMain.print("I love talking about insects.");
+		ChatbotMain.print("I love talking about insects. What is your favorite insect?");
 		response = ChatbotMain.getInput();
-		isGibberish(response);
-		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
-			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
-				ChatbotMain.print("I can't even. I love pugs so much. Wow. You are so cool.");
-				response = ChatbotMain.getInput();
-			}else {
-				ChatbotMain.print("Yeah. That's pretty cool. But there are things I like even more. Tell me something else.");
-				response = ChatbotMain.getInput();
-			}
+		favInsect = response;
+		if(isGibberish(response)) {
+			ChatbotMain.print("Hmm, interesting. I've never heard of " + response + " before.");
+		} else {
+			ChatbotMain.print("I think " + getFavInsect() + " are cool too.");
 		}
-		ChatbotMain.print("Well, it was nice talking to you, " + ChatbotMain.chatbot.getUsername() + "!");
+		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
+				ChatbotMain.print("What would you like to know about insects?");
+				response = ChatbotMain.getInput();
+				if(isInsultFound(response)) {
+					ChatbotMain.print("Please be nicer to me. Robots have feelings too.");
+				}
+			
+			}
+		if(happyCount > madCount) {
+			ChatbotMain.print("Thanks for the friendly conversation about insects " + ChatbotMain.chatbot.getUsername() + "." + "I hope your love for " + getFavInsect() + " lasts forever.");
+		} else {
+			ChatbotMain.print("Finally.. I hate talking with people who don't know anything about insects. GOOD BYE " + ChatbotMain.chatbot.getUsername());
+		}
 		ChatbotMain.chatbot.startChatting();
 	}
 
@@ -65,6 +83,18 @@ public class ChatbotRaymond implements Topic {
 			}
 		}
 		return true;
+	}
+	public boolean isInsultFound(String response) {
+		for(int i = 0; i < insults.length; i++) {
+			if(response.contains(insults[i])) {
+				madCount ++;
+				return true;
+			}
+		}
+		return false;
+	}
+	public String getFavInsect() {
+		return favInsect;
 	}
 
 }
